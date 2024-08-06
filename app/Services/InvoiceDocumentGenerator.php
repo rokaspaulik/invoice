208c390a18160073e742934f6309baf2d4ad2c5a
rokaspaulikas <rokas.paulikas@gmail.com>
2024-08-06 18:46:47 +0300

    improve code quality

--- app/Services/InvoiceDocumentGenerator.php
+++ app/Services/InvoiceDocumentGenerator.php
@@ -14,7 +14,6 @@ use PhpOffice\PhpWord\SimpleType\TextAlignment;
 
 class InvoiceDocumentGenerator
 {
-    private const FILE_PATH = 'storage/output/';
     private const TABLE_BS_CELL_WIDTH = 5000;
     private const TABLE_BS_ROWS = 10;
     private const TABLE_BS_COLS = 2;
@@ -27,72 +26,88 @@ class InvoiceDocumentGenerator
     public function generate(): void
     {
         $word = new PhpWord();
+
+        $this->generateHeading($word)
+            ->generateSeries($word)
+            ->generateDate($word)
+            ->generateBuyerSellerInfo($word)
+            ->generateInvoiceItemsTable($word)
+            ->generateBuyerSellerCredentials($word)
+            ->generateNotes($word);
+
+        $this->exportAsWord($word);
+    }
+
+    private function generateHeading(PhpWord $word): self
+    {
+        $section = $word->addSection(['breakType' => 'continuous']);
+        $section->addText('SĄSKAITA FAKTŪRA', ['bold' => true, 'size' => 18], ['alignment' => TextAlignment::CENTER, 'spaceAfter' => 240]);
+
+        return $this;
+    }
+
+    private function generateSeries(PhpWord $word): self
+    {
+        $section = $word->addSection(['breakType' => 'continuous']);
+        $section->addText(sprintf('Serija %s Nr. %s', $this->invoice->getSeries(), $this->invoice->getSeriesNumber()), ['size' => 9], ['alignment' => TextAlignment::CENTER]);
+
+        return $this;
+    }
+
+    private function generateDate(PhpWord $word): self
+    {
+        $section = $word->addSection(['breakType' => 'continuous']);
+        $section->addText(sprintf('Sąskaitos data %s', $this->invoice->getDate()->format('Y-m-d'), $this->invoice->getSeriesNumber()), ['size' => 9], ['alignment' => TextAlignment::CENTER]);
+
+        $section = $word->addSection(['breakType' => 'continuous']);
+        $section->addText(sprintf('Apmokėti iki %s', $this->invoice->getDateDue()->format('Y-m-d'), $this->invoice->getSeriesNumber()), ['size' => 9], ['alignment' => TextAlignment::CENTER, 'spaceAfter' => 400]);
+
+        return $this;
+    }
+
+    private function generateBuyerSellerInfo(PhpWord $word): self
+    {
         $sellerInfo = $this->invoice->getSeller()->getInfo();
         $buyerInfo = $this->invoice->getBuyer()->getInfo();
 
-        // Heading section
-        $font = ['bold' => true, 'size' => 18];
-        $paragraph = ['alignment' => TextAlignment::CENTER, 'spaceAfter' => 240];
         $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText('SĄSKAITA FAKTŪRA', $font, $paragraph);
-
-        // Series section
-        $font = ['size' => 9];
-        $paragraph = ['alignment' => TextAlignment::CENTER];
-        $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText(sprintf('Serija %s Nr. %s', $this->invoice->getSeries(), $this->invoice->getSeriesNumber()), $font, $paragraph);
-
-        // Date section
-        $font = ['size' => 9];
-        $paragraph = ['alignment' => TextAlignment::CENTER];
-        $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText(sprintf('Sąskaitos data %s', $this->invoice->getDate()->format('Y-m-d'), $this->invoice->getSeriesNumber()), $font, $paragraph);
-
-        // Date due section
-        $font = ['size' => 9];
-        $paragraph = ['alignment' => TextAlignment::CENTER, 'spaceAfter' => 400];
-        $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText(sprintf('Apmokėti iki %s', $this->invoice->getDateDue()->format('Y-m-d'), $this->invoice->getSeriesNumber()), $font, $paragraph);
-
-        // Buyer and seller table section
         $table = $section->addTable();
         $table->addRow();
 
-        $font = ['bold' => true, 'size' => 9];
         $section = $word->addSection(['breakType' => 'continuous']);
-        $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText('Pardavėjas', $font);
-        $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText('Pirkėjas', $font);
+        $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText('Pardavėjas', ['bold' => true, 'size' => 9]);
+        $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText('Pirkėjas', ['bold' => true, 'size' => 9]);
 
-        $font = ['size' => 9];
         for ($row = 1; $row <= self::TABLE_BS_ROWS; ++$row) {
             $table->addRow();
 
             for ($col = 1; $col <= self::TABLE_BS_COLS; ++$col) {
                 if ($col % 2) {
-                    // Add seller info
                     $value = !empty($sellerInfo) ? array_shift($sellerInfo) : '';
-                    $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText($value, $font);
+                    $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText($value, ['size' => 9]);
                     continue;
                 }
-                // Add buyer info
                 $value = !empty($buyerInfo) ? array_shift($buyerInfo) : '';
-                $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText($value, $font);
+                $table->addCell(self::TABLE_BS_CELL_WIDTH)->addText($value, ['size' => 9]);
             }
         }
 
-        // Invoice items table
+        return $this;
+    }
+
+    private function generateInvoiceItemsTable(PhpWord $word): self
+    {
+        $section = $word->addSection(['breakType' => 'continuous']);
         $table = $section->addTable();
         $table->addRow();
 
-        $font = ['bold' => true, 'size' => 9];
         $section = $word->addSection(['breakType' => 'continuous']);
-        $table->addCell(4000)->addText('Pavadinimas', $font);
-        $table->addCell(1000)->addText('Kiekis', $font);
-        $table->addCell(1000)->addText('Matas', $font);
-        $table->addCell(2000)->addText('Kaina', $font);
-        $table->addCell(2000)->addText('Iš viso', $font);
+        $table->addCell(4000)->addText('Pavadinimas', ['bold' => true, 'size' => 9]);
+        $table->addCell(1000)->addText('Kiekis', ['bold' => true, 'size' => 9]);
+        $table->addCell(1000)->addText('Matas', ['bold' => true, 'size' => 9]);
+        $table->addCell(2000)->addText('Kaina', ['bold' => true, 'size' => 9]);
+        $table->addCell(2000)->addText('Iš viso', ['bold' => true, 'size' => 9]);
 
-        $font = ['size' => 9];
         foreach ($this->invoice->getItems() as $item) {
             $table->addRow();
 
@@ -109,54 +124,54 @@ class InvoiceDocumentGenerator
 
             $itemTotalMoneyObject = MoneyHandler::calculateInvoiceItemTotal($invoiceItem);
 
-            $table->addCell(2000)->addText($invoiceItem->getName(), $font);
-            $table->addCell(2000)->addText($invoiceItem->getAmount(), $font);
-            $table->addCell(2000)->addText($invoiceItem->getUnit(), $font);
-            $table->addCell(2000)->addText(MoneyHandler::format($invoiceItem->getMoney()), $font);
-            $table->addCell(2000)->addText(MoneyHandler::format($itemTotalMoneyObject), $font);
+            $table->addCell(2000)->addText($invoiceItem->getName(), ['size' => 9]);
+            $table->addCell(2000)->addText($invoiceItem->getAmount(), ['size' => 9]);
+            $table->addCell(2000)->addText($invoiceItem->getUnit(), ['size' => 9]);
+            $table->addCell(2000)->addText(MoneyHandler::format($invoiceItem->getMoney()), ['size' => 9]);
+            $table->addCell(2000)->addText(MoneyHandler::format($itemTotalMoneyObject), ['size' => 9]);
         }
 
-        // Total section
         $invoiceCents = MoneyHandler::calculateInvoiceTotalInCents($this->invoice);
         $money = MoneyHandler::fromCents($invoiceCents);
         $table->addRow();
         $table->addCell(8000)->addText('Bendra suma', ['bold' => true, 'size' => 9], ['spaceBefore' => 400, 'alignment' => Jc::END]);
         $table->addCell(1000)->addText(MoneyHandler::format($money), ['size' => 9], ['spaceBefore' => 400]);
 
-        // Money words section
         $moneyWords = MoneyHandler::moneyToWords($money);
-        $font = ['size' => 9];
-        $paragraph = ['spaceBefore' => 1000, 'spaceAfter' => 500];
         $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText(sprintf('Suma žodžiais: %s', $moneyWords), $font, $paragraph);
+        $section->addText(sprintf('Suma žodžiais: %s', $moneyWords), ['size' => 9], ['spaceBefore' => 1000, 'spaceAfter' => 500]);
 
-        // Seller credentials section
-        $font = ['size' => 9];
-        $paragraph = ['spaceAfter' => 500];
+        return $this;
+    }
+
+    private function generateBuyerSellerCredentials(PhpWord $word): self
+    {
         $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText(sprintf('Sąskaitą išrašė: %s', $this->invoice->getSeller()->getName()), $font, $paragraph);
+        $section->addText(sprintf('Sąskaitą išrašė: %s', $this->invoice->getSeller()->getName()), ['size' => 9], ['spaceAfter' => 500]);
 
-        // Buyer credentials section
-        $font = ['size' => 9];
-        $paragraph = ['spaceAfter' => 500];
         $section = $word->addSection(['breakType' => 'continuous']);
-        $section->addText('Sąskaitą priėmė: _______________________________', $font, $paragraph);
+        $section->addText('Sąskaitą priėmė: _______________________________', ['size' => 9], ['spaceAfter' => 500]);
 
-        // Notes section
+        return $this;
+    }
+
+    private function generateNotes(PhpWord $word): self
+    {
+        $section = $word->addSection(['breakType' => 'continuous']);
         $section->addText('Pastabos', ['size' => 9, 'bold' => true], ['spaceAfter' => 100]);
+
         $section = $word->addSection(['breakType' => 'continuous']);
         $section->addText($this->invoice->getNotes(), ['size' => 9]);
 
-        $this->exportAsWord($word);
+        return $this;
     }
 
     private function exportAsWord(PhpWord $word): void
     {
-        $path = self::FILE_PATH;
+        $path = 'storage/output/';
         $filename = 'output';
         $extension = '.docx';
 
-        (IOFactory::createWriter($word, 'Word2007'))
-            ->save(sprintf('%s%s%s', $path, $filename, $extension));
+        (IOFactory::createWriter($word, 'Word2007'))->save(sprintf('%s%s%s', $path, $filename, $extension));
     }
 }
